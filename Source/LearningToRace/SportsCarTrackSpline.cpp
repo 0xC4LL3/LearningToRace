33b506fc673191584ca25d5c9f5ae0a339d0443a
0xC4LL3 <76694291+0xC4LL3@users.noreply.github.com>
2025-05-19 15:50:30 +0200

    added track spline for training observations

---
+++ Source/LearningToRace/SportsCarTrackSpline.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SportsCarTrackSpline.h"
+
+// Sets default values
+ASportsCarTrackSpline::ASportsCarTrackSpline()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TrackSpline = CreateDefaultSubobject<USplineComponent>(TEXT("TrackSpline"));
+}
+
+// Called when the game starts or when spawned
+void ASportsCarTrackSpline::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASportsCarTrackSpline::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
