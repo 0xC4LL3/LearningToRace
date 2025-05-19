fd2e563f0d09e384d78b75f6a8f5d22a44ac1fb4
0xC4LL3 <76694291+0xC4LL3@users.noreply.github.com>
2025-05-19 15:57:14 +0200

    implemented learning manager specifying training policy and critic for PPO

---
+++ Source/LearningToRace/SportsCarManager.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SportsCarManager.h"
+
+// Sets default values
+ASportsCarManager::ASportsCarManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ASportsCarManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ALearningToRaceSportsCar::StaticClass(), CarActors);
+	for (AActor* Car : CarActors)
+	{
+		AddTickPrerequisiteActor(Car);
+	}
+
+	TArray<AActor*> OutTracks;
+	UGameplayStatics::GetAllActorsWithTag(GetWorld(), TEXT("TrackSpline"), OutTracks);
+	TrackSpline = Cast<USplineComponent>(OutTracks[0]->GetComponentByClass(USplineComponent::StaticClass()));
+
+    ULearningAgentsManager* Manager = Cast<ALearningToRaceGameMode>(UGameplayStatics::GetGameMode(this))->LearningAgentsManager;
+	Interactor = ULearningAgentsInteractor::MakeInteractor(Manager, USportsCarInteractor::StaticClass(), TEXT("SportsCarInteractor"));
+	Cast<USportsCarInteractor>(Interactor)->TrackSpline = TrackSpline;
+
+	Policy = ULearningAgentsPolicy::MakePolicy(Manager, Interactor, ULearningAgentsPolicy::StaticClass(), TEXT("SportsCarPolicy"), EncoderNetwork, PolicyNetwork, DecoderNetwork);
+	Critic = ULearningAgentsCritic::MakeCritic(Manager, Interactor, Policy, ULearningAgentsCritic::StaticClass(), TEXT("SportsCarCritic"), CriticNetwork);
+	Environment = ULearningAgentsTrainingEnvironment::MakeTrainingEnvironment(Manager, USportsCarTrainingEnv::StaticClass());
+	Cast<USportsCarTrainingEnv>(Environment)->TrackSpline = TrackSpline;
+
+	TrainerProcess = ULearningAgentsCommunicatorLibrary::SpawnSharedMemoryTrainingProcess();
+	Communicator = ULearningAgentsCommunicatorLibrary::MakeSharedMemoryCommunicator(TrainerProcess);
+	PPOTrainer = ULearningAgentsPPOTrainer::MakePPOTrainer(Manager, Interactor, Environment, Policy, Critic, Communicator, ULearningAgentsPPOTrainer::StaticClass());
+
+	/*for (AActor* Car : CarActors)
+	{
+		Cast<ALearningToRaceSportsCar>(Car)->ResetToRandomPointOnSpline(TrackSpline);
+	}*/
+}
+
+// Called every frame
+void ASportsCarManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	PPOTrainer->RunTraining();
+}
---
+++ Source/LearningToRace/SportsCarManager.h
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LearningToRaceSportsCar.h"
+#include "LearningAgentsPolicy.h"
+#include "LearningAgentsCritic.h"
+#include "LearningAgentsTrainingEnvironment.h"
+#include "LearningAgentsCommunicator.h"
+#include "LearningAgentsPPOTrainer.h"
+#include "LearningAgentsNeuralNetwork.h"
+#include "SportsCarInteractor.h"
+#include "SportsCarTrainingEnv.h"
+#include "SportsCarManager.generated.h"
+
+UCLASS()
+class LEARNINGTORACE_API ASportsCarManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Learning")
+	ULearningAgentsNeuralNetwork* EncoderNetwork;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Learning")
+	ULearningAgentsNeuralNetwork* PolicyNetwork;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Learning")
+	ULearningAgentsNeuralNetwork* DecoderNetwork;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Learning")
+	ULearningAgentsNeuralNetwork* CriticNetwork;
+
+	// Sets default values for this actor's properties
+	ASportsCarManager();
+
+private:
+	UPROPERTY()
+	TArray<AActor*> CarActors;
+	UPROPERTY()
+	USplineComponent* TrackSpline;
+	UPROPERTY()
+	ULearningAgentsInteractor* Interactor;
+	UPROPERTY()
+	ULearningAgentsPolicy* Policy;
+	UPROPERTY()
+	ULearningAgentsCritic* Critic;
+	UPROPERTY()
+	ULearningAgentsTrainingEnvironment* Environment;
+	UPROPERTY()
+	FLearningAgentsTrainerProcess TrainerProcess;
+	UPROPERTY()
+	FLearningAgentsCommunicator Communicator;
+	UPROPERTY()
+	ULearningAgentsPPOTrainer* PPOTrainer;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
