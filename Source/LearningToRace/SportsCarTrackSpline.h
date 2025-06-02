f837f60fae8e1d4126974162384a2a5dcbe3e799
0xC4LL3 <76694291+0xC4LL3@users.noreply.github.com>
2025-06-02 12:47:10 +0200

    added copyright notice in project settings and source code files

--- Source/LearningToRace/SportsCarTrackSpline.h
+++ Source/LearningToRace/SportsCarTrackSpline.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright (c) 2025 0xC4LL3. Licensed under the MIT License. See LICENSE file for details.
 
 #pragma once
 
